503bced1683a6208abce06161f305d480328fd80
Diego Boyatjian <dboyatjian@gmail.com>
2023-02-21 13:36:00 -0300

    linkedList append() y prepend()

--- LinkedList/singly.js
+++ LinkedList/singly.js
@@ -1,4 +1,4 @@
-1-- > 2-- > 3-- > 4-- > 5-- > null;
+/*1-- > 2-- > 3-- > 4-- > 5-- > null;
 
 let singlyLinkedList = {
   head: {
@@ -14,25 +14,56 @@ let singlyLinkedList = {
       },
     },
   },
-};
+};*/
 
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class MySinglyLinkedList {
-  constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
-    this.tail = this.head;
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+        };
+        this.tail = this.head;
 
-    this.length = 1;
-  }
+        this.length = 1;
+    }
+
+    /* agrega un nodo al final */
+    append(value) {
+        const newNode = new Node(value);
+        
+        // agregamos el nodo a la lista
+        this.tail.next = newNode;
+        // hacemos que el nuevo nodo sea el ultimo nodo
+        this.tail = newNode;
+        this.length++;
+
+        return this;
+    }
+
+    /* agrega un nodo al inicio */
+    prepend(value) {
+        const newNode = new Node(value);
+
+        // agregamos el nodo a la lista
+        newNode.next = this.head;
+        // hacemos que el nuevo nodo sea el primer nodo
+        this.head = newNode;
+        this.length++;
+
+        return this;
+    }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
+myLinkedList.append(2);
+myLinkedList.append(3);
+myLinkedList.append(4);
+myLinkedList.prepend(0);
+console.log(myLinkedList)
