e2ad2a173df1b73d8d07150f7aa31606c1105975
Diego Boyatjian <dboyatjian@gmail.com>
2023-02-22 10:44:01 -0300

    doubly Linked List, to do : methods

---
+++ LinkedList/doubly.js
@@ -0,0 +1,40 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+      this.prev = null;
+    }
+  }
+  class MyDoublyLinkedList {
+    constructor(value) {
+      this.head = {
+        value: value,
+        next: null,
+        prev: null,
+      };
+      this.tail = this.head;
+  
+      this.length = 1;
+    }
+    append(value) {
+      const newNode = new Node(value);
+      newNode.prev = this.tail; // esto es lo nuevo
+      this.tail.next = newNode;
+      this.tail = newNode;
+  
+      this.length++;
+  
+      return this;
+    }
+  }
+  
+  let myDoublyLinkedList = new MyDoublyLinkedList(1);
+
+  /*
+  to do:
+  prepend()
+  remove()
+  search()
+  insert()
+
+  */
