ab9006e48d9bfb12fbc87340cb0b88c36bbede69
Diego Boyatjian <dboyatjian@gmail.com>
2023-02-22 02:37:49 -0300

    insert, search y remove para linkedList

--- LinkedList/singly.js
+++ LinkedList/singly.js
@@ -59,6 +59,58 @@ class MySinglyLinkedList {
 
         return this;
     }
+
+    // saca un nodo, y reacomoda todo para que quede bien
+    remove(index){
+        if (index < 0 || index > this.length){
+            throw Error('error al remover elemento, revisr el index solicitado');
+        }
+        
+        const nodoAnterior = this.searchIndex(index -1);
+        const nodoRemovido = nodoAnterior.next;
+        const nodoPosterior = nodoRemovido.next;
+        nodoAnterior.next = nodoPosterior;
+        this.length--;
+        
+        console.log('el nodo removido es: ' + nodoRemovido.value);
+        // para que el elemento retornado no tenga referencias a nodos
+        nodoRemovido.next = null; 
+        return nodoRemovido;
+    }
+
+    /* agrega un nodo en una posicion especifica*/
+    insert(index, value){
+        // si el index pasado es mayor a la lognitud de la lista se agrega al final
+        if (index >= this.length){
+            return this.append(value);
+        }
+
+        // si el index es 0, agrega el nodo al inicio
+        if (index === 0){
+            return this.prepend(value);
+        }
+
+        const newNode = new Node(value);
+        // busca el nodo anterior al solicitado
+        const nodoAnterior = this.searchIndex(index - 1);
+        const nodoPosterior = nodoAnterior.next;
+        nodoAnterior.next = newNode;
+        newNode.next = nodoPosterior;
+        this.length++;
+        return this;
+    }
+
+    /* busca el nodo segun el index */
+    searchIndex(index){
+        let counter = 0;
+        let currentNode = this.head;
+
+        while(counter !== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
@@ -66,4 +118,7 @@ myLinkedList.append(2);
 myLinkedList.append(3);
 myLinkedList.append(4);
 myLinkedList.prepend(0);
+myLinkedList.insert(4, 3.5);
 console.log(myLinkedList)
+let removido = myLinkedList.remove(4);
+console.log(removido);
